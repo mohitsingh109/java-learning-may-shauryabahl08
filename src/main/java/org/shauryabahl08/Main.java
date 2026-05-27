d7d8edddfa5e4766dc90930d8517609f0ccdab30
mohitsingh <iammohitsingh109@gmail.com>
2026-05-27 15:33:18 +0530

    Initial commit

---
+++ src/main/java/org/shauryabahl08/Main.java
@@ -0,0 +1,63 @@
+package org.shauryabahl08;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("shauryabahl08@gmail.com");
+        System.out.println("I study in class 12th");
+
+        // Variable in java
+        //data type Variable = value
+        String fullName = "Shaurya";
+        int productAmount = 200;
+        String address = "H.no 234 bla bla";
+
+        System.out.println(fullName);
+        System.out.println(productAmount);
+        System.out.println(address);
+
+        // String, char, int, long, float, double, boolean
+        char gender = 'M';
+        int age = 1956546546;
+        long bigInt = 4543958349584343534L;
+        float price = 99.99f;
+        double bigPrice = 99.99;
+        boolean isPlayerActive = true; // false or true
+
+        String stuName = "Mohit";
+        String classStd = "12th!@#"; // String ==> to store anything inside "" alphanumeric
+        int rollNo = 34;
+        double marks = 92.5;
+        boolean isPass = true;
+
+        System.out.println("===========Print Label============");
+        System.out.println(stuName);
+        System.out.println(classStd);
+        System.out.println(rollNo);
+        System.out.println(marks);
+        System.out.println(isPass);
+        System.out.println("=======================");
+
+        double scienceMarks = 87.55;
+        double mathMarks = 92.5;
+        double englishMarks = 78.25;
+
+        double totalMarks = scienceMarks + mathMarks + englishMarks;
+
+        System.out.println("Total Marks: " + totalMarks);
+
+
+    }
+}
+
+// public class Main ?
+// This is the name of our program
+
+
+// main?
+// This is the starting point
+// main gate or entrance of your house
+
+// System.out.println
+// compute spark thought this
