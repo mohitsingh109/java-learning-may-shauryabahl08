c6653aae60f25b46edd8f38af98064ece9e90046
mohitsingh <iammohitsingh109@gmail.com>
2026-06-03 16:21:15 +0530

    MINI SHOPPING BILL CALCULATOR

---
+++ src/main/java/org/shauryabahl08/assignment/Shopping.java
@@ -0,0 +1,36 @@
+package org.shauryabahl08.assignment;
+
+public class Shopping {
+    /*
+     * MINI SHOPPING BILL CALCULATOR
+     *
+     * Create a Java program that works like a shopping bill calculator.
+     *
+     * Requirements:
+     * 1. For product:
+     *      - Enter product name.
+     *      - Enter product price.
+     *      - Enter quantity.
+     * 2. Calculate the total cost of the product.
+     * 3. Apply discount rules:
+     *      - If bill is above ₹5000, give 10% discount.
+     *      - If bill is above ₹10000, give 15% discount.
+     * 4. Display:
+     *      - Product detail
+     *      - Subtotal
+     *      - Discount amount
+     *      - Final payable amount
+     *
+     * Example:
+     *
+     * Product Name: Laptop Bag
+     * Price: 1000
+     * Quantity: 2
+     *
+     *
+     * Subtotal = 2000
+     * Discount = 0
+     * Final Amount = 2000
+     *
+     */
+}
