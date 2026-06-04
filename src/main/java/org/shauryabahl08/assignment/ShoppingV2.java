f3bd493d2413133af45136af1218f4200dff716a
mohitsingh <iammohitsingh109@gmail.com>
2026-06-04 16:33:46 +0530

    MINI SHOPPING BILL CALCULATOR

---
+++ src/main/java/org/shauryabahl08/assignment/ShoppingV2.java
@@ -0,0 +1,42 @@
+package org.shauryabahl08.assignment;
+
+public class ShoppingV2 {
+
+    /*
+     * MINI SHOPPING BILL CALCULATOR
+     *
+     * Create a Java program that works like a shopping bill calculator.
+     *
+     * Requirements:
+     * 1. Ask the user how many products they want to purchase.
+     * 2. For each product:
+     *      - Enter product name.
+     *      - Enter product price.
+     *      - Enter quantity.
+     * 3. Calculate the total cost of each product.
+     * 4. Calculate the grand total of all products.
+     * 5. Apply discount rules:
+     *      - If bill is above ₹5000, give 10% discount.
+     *      - If bill is above ₹10000, give 15% discount.
+     * 6. Display:
+     *      - Product details
+     *      - Subtotal
+     *      - Discount amount
+     *      - Final payable amount
+     *
+     * Example:
+     *
+     * Product 1: Laptop Bag
+     * Price: 1000
+     * Quantity: 2
+     *
+     * Product 2: Mouse
+     * Price: 500
+     * Quantity: 3
+     *
+     * Subtotal = 3500
+     * Discount = 0
+     * Final Amount = 3500
+     *
+     */
+}
---
+++ src/main/java/org/shauryabahl08/loops/LoopsInJava.java
@@ -0,0 +1,68 @@
+package org.shauryabahl08.loops;
+
+import java.util.Scanner;
+
+public class LoopsInJava {
+
+    public static void main(String[] args) {
+        // Loops stuff in java
+        // Anything that is a repeat process we put in loop
+
+        // for, while, do-while
+
+        // 1000 - Hello
+        //for(init; <condition>; step operation)
+        // bottom to up (step up loop)
+//        for(int i = 0; i < 1000 ; i++) {
+//            System.out.println("Hello - " + i); // i = 1000 Hello - 0, Hello - 1, Hello - 2
+//        }
+
+        // top to bottom (step down loop)
+        // 1000, 999, 998 .... 2, 1
+//        for(int i = 1000; i > 0; i--) {
+//            System.out.println(i);
+//        }
+
+//        for(int i = 0; i <= 50; i += 3) {
+//            System.out.println(i);
+//        }
+
+//        for(int i = 0, j = 0; i < 10 && j < 10; i++, j++) {
+//            System.out.println("(I: " + i + " ) (J: " + j + " )");
+//        }
+
+        // How many number user want to input
+        // Add the validation that number should be > 0
+        // we need to run the loop count upto the number user entered
+        // take the total sum
+
+        // How many number you need to enter? 3 (user)
+        // take 3 time input from the user
+        // input 1: 10 (user)
+        // input 2: 20 (user)
+        // input 3: 30 (user)
+        // Sum: 60
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many number you need to enter?");
+        int count = sc.nextInt(); // 5
+        int sum = 20;
+        // 0 < count <= 10 - Python support this
+        if (count > 0 && count <= 10) {
+            for (int i = 1; i <= count; i++) {
+                System.out.println("Input " + i + ":");
+                int input = sc.nextInt(); // hand
+                //sum += input; // sum = sum + input
+                sum = sum + input; // sum += input;
+                System.out.println("You input: " + input + " Current sum: " + sum);
+            }
+
+            System.out.println("Sum : " + sum);
+        } else {
+            System.out.println("Number should be between 1 to 10");
+        }
+
+        System.out.println("I'm outside the loop");
+
+    }
+}
